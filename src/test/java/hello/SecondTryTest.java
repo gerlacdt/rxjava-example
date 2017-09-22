2e392e32cde016aaa946f5013577ff22b911e97f
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2017-09-22 23:39:01 +0200

    updated tests, get rid of deprecated function calls

--- src/test/java/hello/SecondTryTest.java
+++ src/test/java/hello/SecondTryTest.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -16,125 +15,100 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import rx.Observable;
+import rx.Subscriber;
 
 public class SecondTryTest {
-	
+
 	private static ExecutorService pool;
-	
+
 	Logger LOGGER = LoggerFactory.getLogger(SecondTryTest.class);
-	
+
 	@BeforeClass
 	public static void setUpClass() {
 		pool = Executors.newFixedThreadPool(10);
 	}
-	
+
 	@AfterClass
 	public static void tearDownClass() {
 		pool.shutdown();
 	}
-	
+
 	@Test
 	public void foo1Test() {
-		
+
 		Observable<String> data = getData();
-		data.map((s) -> {return s +  " daniel";})
-			.subscribe(s -> System.out.println(s));
+		data.map((s) -> {
+			return s + " daniel";
+		}).subscribe(s -> System.out.println(s));
 	}
-	
+
 	@Test
 	public void foo2Test() {
 		Observable<String> from = Observable.from(Arrays.asList("a", "b", "c", "ab", "cb", "ba"));
-		from.map(s -> "DEBUG: " + s)
-			.filter(s -> s.contains("a"))
-			.subscribe(System.out::println);
-		
+		from.map(s -> "DEBUG: " + s).filter(s -> s.contains("a")).subscribe(System.out::println);
+
 	}
-	
+
 	@Test
 	public void foo3Test() throws InterruptedException {
-		final CountDownLatch latch = new CountDownLatch(1);
-		getDataSlow().subscribe(number -> {
+		getDataSlow().toBlocking().subscribe(number -> {
 			LOGGER.debug("random generate number: " + number);
-			latch.countDown();
-		},
-		ex -> {
+		}, ex -> {
 			LOGGER.warn("ex was thrown: ", ex);
 		});
-		latch.await();
 	}
-	
+
 	@Test
 	public void observableMergeTest() throws InterruptedException {
-		
-		// observables must have the same type output
-		
-		final CountDownLatch latch = new CountDownLatch(2);
 		Observable<Integer> dataSlow = getDataSlow();
 		Observable<Integer> dataSlow2 = getDataSlow();
-		
-		Observable.merge(dataSlow, dataSlow2)
-				  .subscribe(element -> {
-					  LOGGER.debug("merged observable numbers: " + element);
-					  latch.countDown();
-				  });
-		
-		latch.await();
+
+		Observable.merge(dataSlow, dataSlow2).toBlocking().subscribe(element -> {
+			LOGGER.debug("merged observable numbers: " + element);
+		});
 	}
-	
+
 	@Test
 	public void observableZipTest() throws InterruptedException {
-		
-		// observables type outputs can differ
-		
-		final CountDownLatch latch = new CountDownLatch(1);
 		Observable<Integer> dataSlow = getDataSlow();
 		Observable<String> dataSlow2 = getDataSlow2();
-		
-		Observable.zip(dataSlow, dataSlow2, (d1, d2) -> Arrays.asList(d1, d2))
-				  .subscribe(element -> {
-					  LOGGER.debug("merged observable numbers: " + element);
-					  latch.countDown();
-				  });
-		
-		latch.await();
+		Observable.zip(dataSlow, dataSlow2, (d1, d2) -> Arrays.asList(d1, d2)).toBlocking().subscribe(element -> {
+			LOGGER.debug("merged observable numbers: " + element);
+		});
 	}
-	
+
 	@Test
 	public void observablesInLoopTest() throws InterruptedException {
-		final CountDownLatch latch = new CountDownLatch(1);
-		
 		long startTime = System.currentTimeMillis();
-		
 		List<Observable<Integer>> os = new ArrayList<Observable<Integer>>();
-		
 		for (int i = 0; i < 10; i++) {
 			os.add(getDataSlow());
 		}
-		
-		Observable.merge(os).subscribe(elements -> {
+		Observable.merge(os).toBlocking().subscribe(elements -> {
 			LOGGER.debug("OUTPUT from merged observable numbers: " + elements);
-			latch.countDown();
 		});
-		
-		latch.await();
-		LOGGER.debug("duration: " + (System.currentTimeMillis() - startTime) + "msec"); 
+		LOGGER.debug("duration: " + (System.currentTimeMillis() - startTime) + "msec");
 	}
-	
+
 	private Observable<String> getData() {
-		return Observable.create(observer -> {
-			observer.onNext("hello");
-			observer.onCompleted();
-		}); 
+		return Observable.unsafeCreate(new Observable.OnSubscribe<String>() {
+			@Override
+			public void call(Subscriber<? super String> observer) {
+				observer.onNext("hello");
+				observer.onCompleted();
+			}
+		});
 	}
-	
+
 	private Observable<Integer> getDataSlow() {
-		return Observable.create(aSubscriber -> {
+		return Observable.unsafeCreate(aSubscriber -> {
 			try {
 				pool.submit(() -> {
 					try {
 						Thread.sleep(500);
 						Random rand = new Random();
 						aSubscriber.onNext(rand.nextInt(1000));
+						aSubscriber.onCompleted();
 					} catch (InterruptedException ex) {
 						LOGGER.warn("interupted exception: ", ex);
 						aSubscriber.onError(ex);
@@ -146,14 +120,15 @@ public class SecondTryTest {
 			}
 		});
 	}
-	
+
 	private Observable<String> getDataSlow2() {
-		return Observable.create(aSubscriber -> {
+		return Observable.unsafeCreate(aSubscriber -> {
 			try {
 				pool.submit(() -> {
 					try {
 						Thread.sleep(1000);
 						aSubscriber.onNext(UUID.randomUUID().toString());
+						aSubscriber.onCompleted();
 					} catch (InterruptedException ex) {
 						LOGGER.warn("interupted exception: ", ex);
 						aSubscriber.onError(ex);
@@ -165,5 +140,5 @@ public class SecondTryTest {
 			}
 		});
 	}
-	
+
 }
